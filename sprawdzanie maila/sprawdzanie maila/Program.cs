0185db01205b455e084f4624eb75b621ce86dbea
HroBiT <78382357+HroBiT@users.noreply.github.com>
2024-04-10 09:27:42 +0200

    Add files via upload

---
+++ sprawdzanie maila/sprawdzanie maila/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj maila do sprawdzenia");
+        var mail = Console.ReadLine();
+
+        var pattern = @"\A[a-z0-9]+([-._][a-z0-9]+)*@([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,4}\z";
+
+        var hej = Regex.IsMatch(mail, pattern);
+
+        Console.WriteLine(hej);
+
+    }
+}
